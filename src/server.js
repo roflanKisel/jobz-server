1fc3fd3639869492526053a815ca4091a7bc6602
alexander.kiseliov <kiselev20060@gmail.com>
2018-09-16 14:07:28 +0300

    add sequelize

--- src/server.js
+++ src/server.js
@@ -1,7 +1,19 @@
 import http from 'http';
+import Sequelize from 'sequelize';
 import app from './app';
 import config from './config';
 import logger from './utils/logger';
 
-http.createServer(app.callback()).listen(config.env.PORT);
-logger.log('info', `Server is running on port ${config.env.PORT}\n`);
+const sequelize = new Sequelize('jobzdb', 'root', 'm8463854', {
+  host: 'localhost',
+  dialect: 'mysql',
+});
+
+sequelize.sync()
+  .then(() => {
+    http.createServer(app.callback()).listen(config.env.PORT);
+    logger.log('info', `Server is running on port ${config.env.PORT}\n`);
+  })
+  .catch((err) => {
+    logger.log('error', err);
+  });
