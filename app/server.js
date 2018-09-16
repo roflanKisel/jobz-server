da7422036e2319ed238659c3fcd18da1cc77668b
alexander.kiseliov <kiselev20060@gmail.com>
2018-09-16 11:43:06 +0300

    rename app to src

--- app/server.js
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-import app from './app';
-import config from './config';
-import logger from './utils/logger';
-
-http.createServer(app.callback()).listen(config.env.PORT);
-logger.log('info', `Server is running on port ${config.env.PORT}\n`);
