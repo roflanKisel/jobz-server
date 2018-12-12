fdfbefde4f3479fea6b90f8e630cf3f3dd9e97df
Alexander Kiseliov <kiselev20060@gmail.com>
2018-12-12 13:17:16 +0300

    integration tests

--- src/modules/authentication/services/authentication.js
+++ src/modules/authentication/services/authentication.js
@@ -2,107 +2,104 @@ import passport from 'koa-passport';
 import { User } from '../../../models';
 import jwtService from './jwt-service';
 
-const createUser = async (userData) => {
-  const { email } = userData;
+class UserService {
+  static async createUser(userData) {
+    const { email } = userData;
 
-  const user = await User.findOne({
-    where: { email },
-  });
-
-  if (!user) {
-    const token = jwtService.getToken(userData);
-    User.create({ ...userData });
-    return {
-      token,
-      user: { ...userData, password: undefined },
-    };
-  }
-
-  throw new Error('User is already exist');
-};
-
-const authenticateUser = async ctx => passport.authenticate('local', (err, user) => {
-  if (user) {
-    const token = jwtService.getToken(ctx.request.body);
-    user.password = undefined;
-    ctx.body = { token, user };
-  } else {
-    ctx.body = { token: false, error: err };
-    ctx.status = 401;
-  }
-})(ctx);
-
-const findUser = async (token) => {
-  const user = jwtService.verify(token);
-  const foundUser = await User.findOne({
-    attributes: { exclude: ['password'] },
-    where: { email: user.email },
-  });
-
-  if (!foundUser) {
-    throw new Error('User is not found');
-  }
-
-  return foundUser;
-};
-
-const findUserById = async (id) => {
-  const foundUser = await User.findOne({
-    attributes: { exclude: ['password'] },
-    where: { id },
-  });
-
-  if (!foundUser) {
-    throw new Error(`User with id: ${id} not found`);
-  }
-
-  return foundUser;
-};
-
-const deleteUserUsingToken = async (token) => {
-  const user = jwtService.verify(token);
-  const foundUser = await User.findOne({
-    where: {
-      email: user.email,
-    },
-  });
-
-  if (foundUser) {
-    return foundUser.destroy();
-  }
-
-  throw new Error('Error deleting user');
-};
-
-const deleteUserUsingId = async (token, id) => {
-  const adminData = jwtService.verify(token);
-  const admin = await User.findOne({
-    where: {
-      email: adminData.email,
-      role: 'admin',
-    },
-  });
-
-  if (admin) {
     const user = await User.findOne({
+      where: { email },
+    });
+
+    if (!user) {
+      const token = jwtService.getToken(userData);
+      User.create({ ...userData });
+      return {
+        token,
+        user: { ...userData, password: undefined },
+      };
+    }
+
+    throw new Error('User is already exist');
+  }
+
+  static async authenticateUser(ctx) {
+    return passport.authenticate('local', (err, user) => {
+      if (user) {
+        const token = jwtService.getToken(ctx.request.body);
+        user.password = undefined;
+        ctx.body = { token, user };
+      } else {
+        ctx.body = { token: false, error: err };
+        ctx.status = 401;
+      }
+    })(ctx);
+  }
+
+  static async findUser(token) {
+    const user = jwtService.verify(token);
+    const foundUser = await User.findOne({
+      attributes: { exclude: ['password'] },
+      where: { email: user.email },
+    });
+
+    if (!foundUser) {
+      throw new Error('User is not found');
+    }
+
+    return foundUser;
+  }
+
+  static async findUserById(id) {
+    const foundUser = await User.findOne({
+      attributes: { exclude: ['password'] },
       where: { id },
     });
 
-    if (user) {
-      return user.destroy();
+    if (!foundUser) {
+      throw new Error(`User with id: ${id} not found`);
     }
 
-    throw new Error(`User with id: ${id} not found`);
+    return foundUser;
   }
 
-  throw new Error('Permission denied');
-};
+  static async deleteUserUsingToken(token) {
+    const user = jwtService.verify(token);
+    const foundUser = await User.findOne({
+      where: {
+        email: user.email,
+      },
+    });
 
-export default {
-  createUser,
-  authenticateUser,
-  findUser,
-  findUserById,
-  deleteUserUsingToken,
-  deleteUserUsingId,
-};
+    if (foundUser) {
+      return foundUser.destroy();
+    }
+
+    throw new Error('Error deleting user');
+  }
+
+  static async deleteUserUsingId(token, id) {
+    const adminData = jwtService.verify(token);
+    const admin = await User.findOne({
+      where: {
+        email: adminData.email,
+        role: 'admin',
+      },
+    });
+
+    if (admin) {
+      const user = await User.findOne({
+        where: { id },
+      });
+
+      if (user) {
+        return user.destroy();
+      }
+
+      throw new Error(`User with id: ${id} not found`);
+    }
+
+    throw new Error('Permission denied');
+  }
+}
+
+export default UserService;
--- test/integration/authentication.test.js
+++ test/integration/authentication.test.js
@@ -1,29 +1,261 @@
-import supertest from 'supertest';
+import request from 'superagent';
 import UserService from '../../src/modules/authentication/services/authentication';
-import { User } from '../../src/models';
+import User from '../../src/models/user';
 import app from '../../src/app';
-import config from '../../src/config';
+import jwtService from '../../src/modules/authentication/services/jwt-service';
+
+
+const API_URL = 'http://localhost:8888/api';
 
 describe('Authentication HTTP Controller', () => {
-  let request;
   let server;
 
-  beforeAll(() => {
-    server = app.listen(8888);
-    request = supertest(server);
+  beforeEach(() => {
+    server = app.listen(8888).on('error', err => console.log(err));
   });
 
-  afterAll(() => {
+  afterEach(() => {
     server.close();
   });
 
-  it('handles GET /signin', () => {
-    const mockUserData = { userData: 'test' };
+  it('handles Unauthorized POST /api/auth/signin', async () => {
+    UserService.authenticateUser = jest.fn().mockImplementation(() => new Error());
 
-    // UserService.authenticateUser = jest.fn().mockResolvedValue(mockUserData);
+    try {
+      await request
+        .post(`${API_URL}/auth/signin`)
+        .set('Accept', 'application/json');
+    } catch (err) {
+      expect(err);
+    }
 
-    const response = request.post('/signin');
+    UserService.authenticateUser.mockRestore();
+  });
 
-    console.log(response.status);
+  it('handles Authorized POST /api/auth/signin', async () => {
+    const mockUserData = {
+      email: 'test',
+      password: 'test',
+    };
+
+    jest.spyOn(UserService, 'authenticateUser').mockImplementation((ctx) => { ctx.body = { token: 'test' }; });
+
+    User.findOne = jest.fn().mockImplementation(() => mockUserData);
+
+    const response = await request
+      .post(`${API_URL}/auth/signin`)
+      .set('Accept', 'application/json')
+      .send(mockUserData);
+
+    expect(response.status).toEqual(200);
+
+    UserService.authenticateUser.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('handles POST signup', async () => {
+    const mockUser = { email: 'email', password: 'password' };
+    User.create = jest.fn().mockImplementation(() => {});
+    jest.spyOn(User, 'findOne').mockResolvedValue(null);
+
+    const response = await request
+      .post(`${API_URL}/auth/signup`)
+      .set('Accept', 'application/json')
+      .send(mockUser);
+
+    expect(response.status).toEqual(200);
+
+    User.create.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('rejects an error on POST signup', async () => {
+    const mockUser = { email: 'email', password: 'password' };
+    User.create = jest.fn().mockImplementation(() => {});
+    User.findOne = jest.fn().mockResolvedValue({});
+
+    try {
+      await request
+        .post(`${API_URL}/auth/signup`)
+        .set('Accept', 'application/json')
+        .send(mockUser);
+    } catch (err) {
+      expect(err);
+    }
+
+    User.create.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('finds user by token', async () => {
+    const mockUser = { email: 'test' };
+    UserService.findUser = jest.fn().mockResolvedValue(mockUser);
+
+    const response = await request
+      .post(`${API_URL}/auth/private`)
+      .set('Accept', 'application/json')
+      .send({ token: 'token' });
+
+    expect(response.status).toEqual(200);
+    expect(response.body).toEqual(mockUser);
+
+    UserService.findUser.mockRestore();
+  });
+
+  it('rejects an error if user is not found', async () => {
+    UserService.findUser = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
+
+    try {
+      await request
+        .post(`${API_URL}/auth/private`)
+        .set('Accept', 'application/json')
+        .send({ token: 'token' });
+    } catch (err) {
+      expect(err);
+    }
+
+    UserService.findUser.mockRestore();
+  });
+
+  it('deletes user by token', async () => {
+    const mockUser = {
+      destroy: jest.fn(),
+    };
+
+    jwtService.verify = jest.fn().mockReturnValue({});
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    const response = await request
+      .post(`${API_URL}/auth/delete`)
+      .set('Accept', 'application/json')
+      .send({ token: 'token' });
+
+    expect(response.status).toEqual(200);
+    expect(mockUser.destroy).toHaveBeenCalled();
+
+    jwtService.verify.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('rejects an error deleting user by not valid token', async () => {
+    jwtService.verify = jest.fn().mockReturnValue({});
+    User.findOne = jest.fn().mockResolvedValue(null);
+
+    try {
+      await request
+        .post(`${API_URL}/auth/delete`)
+        .set('Accept', 'application/json')
+        .send({ token: 'token' });
+    } catch (err) {
+      expect(err);
+    }
+
+    jwtService.verify.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('deletes user by id', async () => {
+    const mockUser = {
+      destroy: jest.fn(),
+    };
+
+    jwtService.verify = jest.fn().mockReturnValue({});
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    const response = await request
+      .post(`${API_URL}/auth/admin/delete`)
+      .set('Accept', 'application/json')
+      .send({ token: 'token' });
+
+    expect(response.status).toEqual(200);
+    expect(mockUser.destroy).toHaveBeenCalled();
+
+    jwtService.verify.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('rejects an error if user is not admin', async () => {
+    jwtService.verify = jest.fn().mockReturnValue({});
+    User.findOne = jest.fn().mockResolvedValue(null);
+
+    try {
+      await request
+        .post(`${API_URL}/auth/delete`)
+        .set('Accept', 'application/json')
+        .send({ token: 'token' });
+    } catch (err) {
+      expect(err);
+    }
+
+    jwtService.verify.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('creates user when user service is not throws it on sign up', async () => {
+    const mockUser = { email: 'email', password: 'password' };
+    UserService.createUser = jest.fn().mockResolvedValue(mockUser);
+
+    const response = await request
+      .post(`${API_URL}/auth/signup`)
+      .set('Accept', 'application/json')
+      .send(mockUser);
+
+    expect(response.status).toEqual(200);
+
+    User.create.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('rejects an error when user service throws it on sign up', async () => {
+    const mockUser = { email: 'email', password: 'password' };
+    UserService.createUser = jest.fn().mockImplementation(() => {
+      throw new Error();
+    });
+
+    try {
+      await request
+        .post(`${API_URL}/auth/signup`)
+        .set('Accept', 'application/json')
+        .send(mockUser);
+    } catch (err) {
+      expect(err);
+    }
+
+    User.create.mockRestore();
+    User.findOne.mockRestore();
+  });
+
+  it('gets an user if it found', async () => {
+    const mockUser = { email: 'test' };
+    jwtService.verify = jest.fn().mockReturnValue(mockUser);
+    User.findOne = jest.fn().mockResolvedValue(mockUser);
+
+    const response = await request
+      .post(`${API_URL}/auth/private`)
+      .set('Accept', 'application/json')
+      .send({ token: 'token' });
+
+    expect(response.status).toEqual(200);
+
+    User.findOne.mockRestore();
+    jwtService.verify.mockRestore();
+  });
+
+
+  it('rejects an error if user not found', async () => {
+    User.findOne = jest.fn().mockResolvedValue(null);
+
+    try {
+      await request
+        .post(`${API_URL}/auth/private`)
+        .set('Accept', 'application/json')
+        .send({ token: 'token' });
+    } catch (err) {
+      expect(err);
+    }
+
+    User.findOne.mockRestore();
   });
 });
